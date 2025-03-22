e44520c2da3598982b7b93278d0c9c04f4eb1c49
Ronaldo Dias <piratagamerzx46@gmail.com>
2025-03-22 15:43:17 -0300

    Added rudimentary main menu and score system, as well as a losing condition

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <windows.h>
 #include <cstdlib>
+#include <string_view>
 
 int clamp(int n, int lower, int upper) {
 	if (n < lower)
@@ -19,9 +20,28 @@ int clamp(int n, int lower, int upper) {
 
 class Tetris
 {
-public:
+private:
 
 	// variables
+
+	// if true, is playing the game, if not, is on menu
+	bool inGame{ false };
+
+	// OFF GAME:
+
+	// variables for main menu
+
+	const std::string_view options[3]
+	{
+		"PLAY",
+		"SETTINGS",
+		"QUIT"
+	};
+	int currentOption{};
+
+	// IN GAME:
+
+	int score{};
 
 	// pointers for random seed
 	int* pTimeSnapshot{};
@@ -350,120 +370,6 @@ public:
 		}
 	}
 
-	void pieceInput(char key)
-	{
-		switch (key)
-		{
-		case 'd':
-
-			movePiece('d');
-
-			// check if overlaping a piece, if so, return and check again.
-			while (1)
-			{
-				bool find{ false };
-
-				for (int y{ yLimit + 1 }; y >= 0 && !find; y--)
-				{
-					for (int x{}; x <= xLimit + 1; x++)
-					{
-						if (pieceCheck(x, y)) {
-							if (plane[x][y] == '[')
-							{
-								movePiece('a');
-
-								find = true;
-							}
-						}
-					}
-				}
-
-				if (!find)
-				{
-					break;
-				}
-			}
-			break;
-
-		case 'a':
-
-			movePiece('a');
-
-			// check if overlaping a piece, if so, return and check again.
-			while (1)
-			{
-				bool find{ false };
-
-				for (int y{ yLimit + 1 }; y >= 0 && !find; y--)
-				{
-					for (int x{}; x <= xLimit + 1; x++)
-					{
-						if (pieceCheck(x, y)) {
-							if (plane[x][y] == '[')
-							{
-								movePiece('d');
-
-								find = true;
-							}
-						}
-					}
-				}
-
-				if (!find)
-				{
-					break;
-				}
-			}
-			break;
-
-		case 's':
-			pieceCoords[1]++;
-
-			// check if overlaping a piece, if so, return and call collision
-			for (int y{ yLimit + 1 }; y >= 0; y--)
-			{
-				for (int x{}; x <= xLimit + 1; x++)
-				{
-					if (pieceCheck(x, y)) {
-						if (plane[x][y] == '[' || y >= yLimit - 1)
-						{
-							pieceCoords[1]--;
-
-							collision();
-
-							break;
-						}
-					}
-				}
-			}
-
-			break;
-
-		case 'w':
-			int pastPiece{ currentPiece };
-
-			spinPiece(currentPiece);
-
-			// check if overlaping a piece, if so, return.
-			for (int y{ yLimit + 1 }; y >= 0; y--)
-			{
-				for (int x{}; x <= xLimit + 1; x++)
-				{
-					if (pieceCheck(x, y)) {
-						if (plane[x][y] == '[')
-						{
-							currentPiece = pastPiece; // revert
-							break;
-						}
-					}
-				}
-			}
-
-			break;
-		}
-
-	}
-
 	void glue()
 	{
 		// glue piece on the plane
@@ -525,6 +431,7 @@ public:
 					}
 
 					destroyed = true;
+					score++;
 				}
 			}
 		}
@@ -546,9 +453,68 @@ public:
 		dissolveRows();
 		newRandomPiece();
 		resetCoords();
+		checkLose();
 	}
 
-	void render()
+	void checkLose()
+	{
+		// check to see if any piece reach the top
+
+		for (int x{ 1 }; x < xLimit; x++)
+		{
+			if (plane[x][0] == '[')
+			{
+				lose();
+			}
+		}
+	}
+
+	void lose()
+	{
+		inGame = false;
+	}
+
+	void cleanPlane()
+	{
+		// remove pieces
+
+		for (int x{}; x < xLimit; x++)
+		{
+			for (int y{}; y < yLimit; y++)
+			{
+				if (plane[x][y] == '[')
+				{
+					plane[x][y] = blank;
+				}
+			}
+		}
+	}
+
+	void reset()
+	{
+		newRandomPiece();
+		resetCoords();
+		cleanPlane();
+		score = 0;
+	}
+
+	void select()
+	{
+		// when select button is pressed in menu
+
+		switch (currentOption)
+		{
+		case 0:
+			inGame = true;
+			reset();
+			break;
+		case 2:
+			exit(0);
+			break;
+		}
+	}
+
+	void renderGame()
 	{
 		// render the game
 
@@ -592,22 +558,259 @@ public:
 			}
 			std::cout << std::endl;
 		}
+	}
 
-		std::cout << pieceCoords[1] - 3;
+	void renderMenu()
+	{
+		// render main menu of the game
+		// the game screen is static on the left, options on the right
+
+		system("cls");
+
+		int i{}; // keep track of what options were printed
+
+		for (int y{}; y < yLimit; y++)
+		{
+			char currentChar{};
+
+			for (int x{}; x < xLimit; x++)
+			{
+				currentChar = plane[x][y];
+
+				// check for piece
+				if ((pieceCoords[0] - 4) < x && x <= pieceCoords[0] &&
+					(pieceCoords[1] - 4) < y && y <= pieceCoords[1])
+				{
+					if (pieces[currentPiece]
+						[x - (pieceCoords[0] - 3)]
+							[y - (pieceCoords[1] - 3)]
+							!= '\0'
+							&& y >= 0)
+					{
+						currentChar = '[';
+					}
+				}
+
+				// really print
+				if (currentChar == '[')
+				{
+					std::cout << "[]";
+				}
+				else if (currentChar != '\0')
+				{
+					std::cout << currentChar << " ";
+				}
+				else
+				{
+					std::cout << "  ";
+				}
+
+
+
+				// print menu stuff at the right side
+
+				if ((y + 1) % 3 == 0 &&                  // margin
+					x == xLimit - 1 &&        // if at the last column
+					i < sizeof(options) / sizeof(options[0]))
+				{
+					char select[3]{ '\0', ' ', ' ' };
+
+					if (i == currentOption)
+					{
+						select[0] = ' ';
+						select[1] = '[';
+						select[2] = ']';
+					}
+
+					
+					std::cout
+						<< "    "
+						<< select[0]
+						<< select[1]
+						<< options[i]
+						<< select[2];
+
+						i++;
+				}
+
+				if (y == 16 && x == xLimit -1)
+				{
+					std::cout
+						<< "     "
+						<< "SCORE: " << score;
+				}
+
+			}
+			std::cout << std::endl;
+		}
+	}
+
+public:
+
+	void takeInput(char key)
+	{
+		if (inGame)
+		{
+			switch (key)
+			{
+			case 'd':
+
+				movePiece('d');
+
+				// check if overlaping a piece, if so, return and check again.
+				while (1)
+				{
+					bool find{ false };
+
+					for (int y{ yLimit + 1 }; y >= 0 && !find; y--)
+					{
+						for (int x{}; x <= xLimit + 1; x++)
+						{
+							if (pieceCheck(x, y)) {
+								if (plane[x][y] == '[')
+								{
+									movePiece('a');
+
+									find = true;
+								}
+							}
+						}
+					}
+
+					if (!find)
+					{
+						break;
+					}
+				}
+				break;
+
+			case 'a':
+
+				movePiece('a');
+
+				// check if overlaping a piece, if so, return and check again.
+				while (1)
+				{
+					bool find{ false };
+
+					for (int y{ yLimit + 1 }; y >= 0 && !find; y--)
+					{
+						for (int x{}; x <= xLimit + 1; x++)
+						{
+							if (pieceCheck(x, y)) {
+								if (plane[x][y] == '[')
+								{
+									movePiece('d');
+
+									find = true;
+								}
+							}
+						}
+					}
+
+					if (!find)
+					{
+						break;
+					}
+				}
+				break;
+
+			case 's':
+				pieceCoords[1]++;
+
+				// check if overlaping a piece, if so, return and call collision
+				for (int y{ yLimit + 1 }; y >= 0; y--)
+				{
+					for (int x{}; x <= xLimit + 1; x++)
+					{
+						if (pieceCheck(x, y)) {
+							if (plane[x][y] == '[' || y >= yLimit - 1)
+							{
+								pieceCoords[1]--;
+
+								collision();
+
+								break;
+							}
+						}
+					}
+				}
+
+				break;
+
+			case 'w':
+				int pastPiece{ currentPiece };
+
+				spinPiece(currentPiece);
+
+				// check if overlaping a piece, if so, return.
+				for (int y{ yLimit + 1 }; y >= 0; y--)
+				{
+					for (int x{}; x <= xLimit + 1; x++)
+					{
+						if (pieceCheck(x, y)) {
+							if (plane[x][y] == '[')
+							{
+								currentPiece = pastPiece; // revert
+								break;
+							}
+						}
+					}
+				}
+
+				break;
+			}
+		}
+		else
+		{
+			switch (key)
+			{
+				// move up
+			case 'w':
+				currentOption = clamp(
+					currentOption - 1,
+					0,
+					(sizeof(options) / sizeof(options[0]) - 1));
+				break;
+
+				// move down
+			case 's':
+				currentOption = clamp(
+					currentOption + 1,
+					0,
+					(sizeof(options) / sizeof(options[0]) - 1));
+				break;
+				// select
+			case ' ':
+				select();
+				break;
+			}
+		}
+
 	}
 
 	void next(int _ellapsedTime)
 	{
 		// main event of the game. Call this every frame.
-		ellapsedTime = _ellapsedTime;
 
-		gravity();
+		if (inGame) // play game normally
+		{
+			ellapsedTime = _ellapsedTime;
 
-		render();
+			gravity();
+
+			renderGame();
+		}
+		else // main menu
+		{
+			ellapsedTime = _ellapsedTime;
+
+			renderMenu();
+		}
 
 	}
 
-
+	// constructor
 	Tetris(int* timeSnapshot)
 	{
 		pTimeSnapshot = timeSnapshot;
@@ -639,7 +842,7 @@ int main()
 	// main loop
 	while (!exit)
 	{
-		tetris.next( ellapsedTime );
+		tetris.next(ellapsedTime);
 
 		// subtract cooldowns
 		if (spinCooldown > 0)
@@ -653,24 +856,28 @@ int main()
 
 
 		// keys
-		if (GetKeyState('A') & 0x8000 && moveCooldown == 0) // left
+		if (GetKeyState(VK_LEFT) & 0x8000 && moveCooldown == 0) // left
 		{
-			tetris.pieceInput('a');
+			tetris.takeInput('a');
 			moveCooldown = moveCooldownDefault;
 		}
-		else if (GetKeyState('D') & 0x8000 && moveCooldown == 0) // right
+		else if (GetKeyState(VK_RIGHT) & 0x8000 && moveCooldown == 0) // right
 		{
-			tetris.pieceInput('d');
+			tetris.takeInput('d');
 			moveCooldown = moveCooldownDefault;
 		}
-		if (GetKeyState('S') & 0x8000 && moveCooldown == 0) // down
+		if (GetKeyState(VK_DOWN) & 0x8000 && moveCooldown == 0) // down
 		{
-			tetris.pieceInput('s');
+			tetris.takeInput('s');
 		}
-		if (GetKeyState('W') & 0x8000 && spinCooldown == 0) // spin
+		if (GetKeyState(VK_UP) & 0x8000 && spinCooldown == 0) // spin
 		{
-			tetris.pieceInput('w');
+			tetris.takeInput('w');
 			spinCooldown = spinCooldownDefault;
+		}
+		if (GetKeyState(VK_SPACE) & 0x8000 && moveCooldown == 0) // space
+		{
+			tetris.takeInput(' ');
 		}
 
 
